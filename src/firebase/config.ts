6402281760d559e435e8d759643165a521de2349
Azis JP <axzisalpha@gmail.com>
2025-10-06 03:39:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1191005763-3307e",
+  "appId": "1:426291301177:web:cdf4d105b692811a5db6e8",
+  "apiKey": "AIzaSyBwl-d9goFFTjNJZ2JitkyTAm3z2b86v5o",
+  "authDomain": "studio-1191005763-3307e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "426291301177"
+};
