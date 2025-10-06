cb507c2857d68ac5aed8237371b3cdc91c00079c
Azis JP <axzisalpha@gmail.com>
2025-10-06 01:59:22 +0000

    saya ingin deploy ke hosting saya apakah aman? tidak ada data yang berub

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPAXuku-CSe1hJch2TaluiwzQm_VJeuwI",
-  authDomain: "lamancerdas.firebaseapp.com",
-  projectId: "lamancerdas",
-  storageBucket: "lamancerdas.firebasestorage.app",
-  messagingSenderId: "312334265416",
-  appId: "1:312334265416:web:472ae5b3998673fade7c7d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREbase_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
