a544815fedd5b8ace722d128bd0c6f8d2d7ea4d4
Azis JP <axzisalpha@gmail.com>
2025-10-08 06:15:57 +0000

    commit

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,11 @@
+// src/firebase/config.ts
+
 export const firebaseConfig = {
-  "projectId": "studio-1191005763-3307e",
-  "appId": "1:426291301177:web:cdf4d105b692811a5db6e8",
-  "apiKey": "AIzaSyBwl-d9goFFTjNJZ2JitkyTAm3z2b86v5o",
-  "authDomain": "studio-1191005763-3307e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "426291301177"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
